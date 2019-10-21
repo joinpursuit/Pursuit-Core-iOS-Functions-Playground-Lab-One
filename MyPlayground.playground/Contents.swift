dca0f9418b7bd19fd09cc9c75c327d0e356e9735
Yuliia Engman <yuliiaengmanpursuit@Yuliias-MacBook-Pro.local>
2019-10-20 20:36:52 -0400

    fully completed lab one

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -43,16 +43,43 @@ let testCasesTwo: [(Double, Double, Double)] = [
     (inputOne: 2.3, inputTwo: 2.03, expectedOutput: 2.03)
 ]
 
-//for (inputOne, inputTwo, expectedOutput) in testCasesTwo {
-//    let output = smallest(of: inputOne, and: inputTwo)
-//    assert(output == expectedOutput, "Was expecting \(expectedOutput) for inputs \(inputOne) and \(inputTwo) but got \(output)")
-//}
+func smallest(of inputOne: Double, and inputTwo: Double) -> Double {
+    if inputOne >= inputTwo {
+        return inputTwo
+    } else {
+        return inputOne
+    }
+}
+    print(smallest(of: 2.3, and: 2.03))
+
+for (inputOne, inputTwo, expectedOutput) in testCasesTwo {
+    let output = smallest(of: inputOne, and: inputTwo)
+    assert(output == expectedOutput, "Was expecting \(expectedOutput) for inputs \(inputOne) and \(inputTwo) but got \(output)")
+}
 
 // Question Three
 
 // Write a function named smallestValue(in:) that takes in an array of Doubles and returns the smallest Double
 
 // Your function here
+
+func smallestValue(in array: [Double]) -> Double {
+    var smallestDouble = array[0]
+    for i in array {
+        var isSmallest = true
+        for j in array {
+            if i > j {
+                isSmallest = false
+            }
+        }
+        if isSmallest {
+            smallestDouble = i
+        }
+    }
+    return smallestDouble
+}
+
+print(smallestValue(in: [-4,-59,-348,-34,-4]))
 
 let testCasesThree: [([Double], Double)] = [
     (input: [1.0,2,3,4,5,5], expectedOutput: 1.0),
@@ -61,10 +88,10 @@ let testCasesThree: [([Double], Double)] = [
     (input: [-4,-59,-348,-34,-4], expectedOutput: -348),
 ]
 
-//for (input, expectedOutput) in testCasesThree {
-//    let output = smallestValue(in: input)
-//    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
-//}
+for (input, expectedOutput) in testCasesThree {
+    let output = smallestValue(in: input)
+    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
+}
 
 
 // Question Four
@@ -73,6 +100,18 @@ let testCasesThree: [([Double], Double)] = [
 
 // Your function here
 
+func occurrances(of char: Character, in word: String) -> Int {
+    let arrayOfChar = Array(word)
+    var countOfChar = 0
+    for letter in arrayOfChar {
+        if letter == char {
+            countOfChar += 1
+        }
+    }
+    return countOfChar
+}
+print(occurrances(of: "E", in: "cApItAlS aRe DiFfErEnT"))
+
 let testCasesFour: [(Character, String, Int)] = [
     (inputOne: "l", inputTwo: "hello", expectedOutput: 2),
     (inputOne: "r", inputTwo: "hello world!", expectedOutput: 1),
@@ -80,10 +119,10 @@ let testCasesFour: [(Character, String, Int)] = [
     (inputOne: "E", inputTwo: "cApItAlS aRe DiFfErEnT", expectedOutput: 2),
 ]
 
-//for (inputOne, inputTwo, expectedOutput) in testCasesFour {
-//    let output = occurrances(of: inputOne, in: inputTwo)
-//    assert(output == expectedOutput, "Was expecting \(expectedOutput) for inputs \(inputOne) and \(inputTwo) but got \(output)")
-//}
+for (inputOne, inputTwo, expectedOutput) in testCasesFour {
+    let output = occurrances(of: inputOne, in: inputTwo)
+    assert(output == expectedOutput, "Was expecting \(expectedOutput) for inputs \(inputOne) and \(inputTwo) but got \(output)")
+}
 
 
 // Question Five
@@ -91,6 +130,19 @@ let testCasesFour: [(Character, String, Int)] = [
 // Write a function called removeNils(from:) that takes an array of optional Ints and returns an array with them unwrapped with any nil values removed.
 
 // Your function here
+var arrayWithoutNil = [Int]()
+
+func removeNils(from optionalArray: [Int?]) -> [Int] {
+    for integer in optionalArray {
+        if integer != nil {
+            let integerInt = integer ?? 0
+           arrayWithoutNil.append(integerInt)
+        }
+    }
+    return arrayWithoutNil
+}
+print(removeNils(from: []))
+
 
 let testCasesFive: [([Int?], [Int])] = [
     (input: [1, nil, 9, nil, 10, nil], expectedOutput: [1,9,10]),
@@ -99,8 +151,8 @@ let testCasesFive: [([Int?], [Int])] = [
     (input: [], expectedOutput: []),
 ]
 
-//for (input, expectedOutput) in testCasesFive {
-//    let output = removeNils(from: input)
-//    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
-//}
+for (input, expectedOutput) in testCasesFive {
+    let output = removeNils(from: input)
+    assert(output == expectedOutput, "Was expecting \(expectedOutput) for input \(input), but got \(output)")
+}
 
